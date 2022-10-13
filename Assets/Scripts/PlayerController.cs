38191c67f782a2288a51cd8fbf787776686bee99
katahalonen <kata.halonen@gmail.com>
2022-10-13 13:22:48 +0300

    Character controller
    Added a character controller and animations accordingly

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D theRB;
+    public float jumpForce;
+
+    private bool isGrounded;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+
+    private bool canDoubleJump;
+
+    private Animator anim;
+    private SpriteRenderer theSR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal movement
+        theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
+
+        // Groundcheck
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+
+        // Jumping
+        if(isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(isGrounded)
+            {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
+            else // Doublejump
+            {
+                if(canDoubleJump)
+                {
+                    theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
+        }
+        
+        // Flip player sprite if moving to left
+        if(theRB.velocity.x < 0)
+        {
+            theSR.flipX = true;
+        }
+        // Flip player sprite if moving to right 
+        else if(theRB.velocity.x > 0)
+        {
+            theSR.flipX = false;
+        }
+
+        // Animations
+        anim.SetFloat("moveSpeed", Mathf.Abs(theRB.velocity.x));
+        anim.SetBool("isGrounded", isGrounded);
+    }
+}
